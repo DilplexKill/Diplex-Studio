1ac6592769702b737285780b47ffe2bb968799a4
Diplex <76635506+DilplexKill@users.noreply.github.com>
2025-03-02 19:44:45 +0100

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mon Site</title>
+</head>
+<body>
+    <h1>Bienvenue sur mon site Unity</h1>
+    <p>Ceci est une page simple pour Unity Publisher.</p>
+</body>
+</html>
